a1111ac764219072c04ddf8b2927aee1b88fc0a4
maxiDeee <honnnnyu@gmail.com>
2026-04-26 19:33:11 -0400

    Add JwtService.java

--- src/main/java/com/qiuzhitech/onlineshopping_09/service/JwtService.java
+++ src/main/java/com/qiuzhitech/onlineshopping_09/service/JwtService.java
@@ -1,8 +1,49 @@
 package com.qiuzhitech.onlineshopping_09.service;
 
+import com.qiuzhitech.onlineshopping_09.config.MyUser;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
 
 @Service
 public class JwtService {
+    @Value("${security.jwt.secret-key}")
+    private String secretKey;
+    @Value("360000")
+    private long jwtExpiration;
 
+    public String encryptUser(MyUser user) {
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("id", user.getId());
+        map.put("email", user.getEmail());
+        map.put("name", user.getName());
+        map.put("age", user.getEmail());
+        return Jwts.builder()
+                .setClaims(map)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpiration))
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+    public String decryptUserName(String token) {
+        Claims body = Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+        return body.getOrDefault("name", "-1").toString();
+    }
+
+    private Key getSigningKey() {
+        byte[] decode = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(decode);
+    }
 }
